0adb26d409f99797cf28eab67ff7c8984f5859b7
Prajwalraj77 <hackingbyraj77@gmail.com>
2026-09-17 16:48:58 +0530

    email changed

--- src/config/contact.ts
+++ src/config/contact.ts
@@ -2,7 +2,7 @@ export const contact = {
   phone: "+918197983277",
   displayPhone: "+91 81979 83277",
   whatsapp: "918197983277",
-  email: "hello@softprefix.com",
+  email: "Softprefix@gmail.com",
   address: "Bangalore, Karnataka, India",
   mapsUrl:
     "https://www.google.com/maps/search/?api=1&query=Bangalore%2C%20Karnataka%2C%20India",
